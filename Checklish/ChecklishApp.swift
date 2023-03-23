a40924180585dbca52af805f38d3b461703ed37b
slee212 <samuellee249@gmail.com>
2023-03-23 17:57:54 +1000

    Initial Commit

---
+++ Checklish/ChecklishApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklishApp.swift
+//  Checklish
+//
+//  Created by Samuel Lee on 23/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChecklishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
