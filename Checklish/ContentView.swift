b3a5c408a85db2321e7bf63bd0ac5af216b7fd86
Samuel Lee <samuellee249@gmail.com>
2023-03-26 12:54:20 +1000

    Updated Read Me and created a static checklist with on tap functionality

--- Checklish/ContentView.swift
+++ Checklish/ContentView.swift
@@ -7,13 +7,20 @@
 
 import SwiftUI
 
+var tasks = [["Monday: ", "Finish assignment", "checkmark"],
+             ["Tuesday: ", "Go shopping", "checkmark"],
+             ["Wednesday: ", "Do laundry", "xmark"]]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Sam")
+        VStack(alignment: .leading) {
+            Text("To Do List:")
+            List {
+                ForEach(tasks, id:\.self) {
+                    task in
+                    ListRowView(item: task)
+                }
+            }
         }
         .padding()
     }
@@ -22,5 +29,24 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ListRowView: View {
+    var item:[String]
+    var body: some View {
+        HStack{
+            Text(item[0])
+                .frame(width:100)
+            Text(item[1])
+            Spacer()
+            Image(systemName: item[2])
+        }.onTapGesture {
+            if(item[2] == "checkmark") {
+                print("\(item[1]) is ticked")
+            } else {
+                print("\(item[1]) is not ticked")
+            }
+        }
     }
 }
--- Checklish/Readme.md
+++ Checklish/Readme.md
@@ -3,5 +3,8 @@
 
 ###I learned:
 - How to connect Xcode to github and create a repository.
-
-
+- How to create and use a for each loop using swift
+- How to create a list and add items through the for each
+- How to use an HStack and export to subview
+- How to retrieve an image I want and implement it
+- How to add an On Tap function
