db1f1de1e174634fa75ec62386ecdffd38c692a6
Samuel Lee <samuellee249@gmail.com>
2023-03-31 17:16:18 +1000

    Edited the EditView to implement the edit mode

--- Checklish/EditView.swift
+++ Checklish/EditView.swift
@@ -5,4 +5,27 @@
 //  Created by Samuel Lee on 31/3/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct EditView: View {
+    @Binding var item: String
+    @State var displayItem: String = ""
+    @Environment(\.editMode) var editMode
+    var body: some View {
+        VStack {
+            if(editMode?.wrappedValue == .active) {
+                HStack {
+                    Image(systemName: "creditcard")
+                    TextField("Input:",text: $displayItem)
+                    Button("Cancel") {
+                        displayItem = item
+                    }
+                }.onAppear {
+                    displayItem = item
+                }.onDisappear{
+                    item = displayItem
+                }
+            }
+        }
+    }
+}
