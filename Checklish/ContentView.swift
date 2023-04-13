d7090a18b02a596c59c69eb8ccbbf0fd70f61a61
Samuel Lee <samuellee249@gmail.com>
2023-04-13 16:30:57 +1000

    Added ability to re-order list of checklists

--- Checklish/ContentView.swift
+++ Checklish/ContentView.swift
@@ -27,8 +27,13 @@ struct ContentView: View {
                         model.lists.remove(atOffsets: idx)
                         model.save() // Save the updated data model
                     }
+                    .onMove { idx, i in
+                        // Move the item from one index to another when dragged and dropped
+                        model.lists.move(fromOffsets: idx, toOffset: i)
+                        model.save()
+                    }
                 }.navigationTitle(myTitle) // Set the navigation title to the value of myTitle
-                .navigationBarItems(trailing: Button("+"){
+                    .navigationBarItems(leading: EditButton(), trailing: Button("+"){
                     // Add a button to add a new placeholder item to the list
                     model.lists.append(Checklist(name: "New List", tasks: [["New Task", "xmark"]]))
                     model.save()
