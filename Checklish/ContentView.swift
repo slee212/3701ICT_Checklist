1cf5e2fc14d676bdc45a6cd930fece405ebeaa16
Samuel Lee <samuellee249@gmail.com>
2023-04-19 13:18:16 +1000

    Added documentation for the ContentView

--- Checklish/ContentView.swift
+++ Checklish/ContentView.swift
@@ -6,18 +6,21 @@
 //
 
 import SwiftUI
-
+/// The structure that creates the view the content is housed in. The main menu screen. Contains a list of checklists retrieved from the DataModel using a SwiftUI 'List'. Furthermore, the ContentView makes use of a NavigationView to add a title, alongside navigation buttons. On the ContentView page, there are buttons for adding a new list and editing the current list. Ultimately, the ContentView serves as the primary user interface for the application.
 struct ContentView: View {
-    @Binding var model: DataModel // A binding property that represents a data model
-    @State var myTitle = "My Lists" // A state property that holds the title of the view
+    /// A binding property that represents a data model
+    @Binding var model: DataModel
+    /// A state property that holds the title of the view
+    @State var myTitle = "My Lists"
     
+    /// The body of the page, where the actual content is housed.
     var body: some View {
         NavigationView() {
             VStack {
                 EditView(item: $myTitle) // A custom view called EditView
                 List {
+                    // A list of items with a navigation link to a ListView for each item
                     ForEach(model.lists.enumerated().map { $0 }, id: \.element) { (index, p) in
-                        // A list of items with a navigation link to a ListView for each item
                         NavigationLink(destination: ListView(clist: $model, count: index)) {
                             Text(p.name) // Display the name property of each item in the list
                         }
@@ -25,7 +28,8 @@ struct ContentView: View {
                     .onDelete { idx in
                         // Handle deletion of items in the list
                         model.lists.remove(atOffsets: idx)
-                        model.save() // Save the updated data model
+                        // Save the updated data model
+                        model.save()
                     }
                     .onMove { idx, i in
                         // Move the item from one index to another when dragged and dropped
