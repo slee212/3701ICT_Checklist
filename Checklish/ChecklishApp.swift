aeace7486e7a4aef1515466e5515354b5f7e1b00
Samuel Lee <samuellee249@gmail.com>
2023-04-19 13:23:51 +1000

    Edited ChecklishApp documentation

--- Checklish/ChecklishApp.swift
+++ Checklish/ChecklishApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 /// The ChecklishApp struct creates an instancee of the DataModel class to provide storage for the application. Furthermore, it provides a way to initialise and manager the user interface of the application.
 struct ChecklishApp: App {
+    /// Creates a variable called model marked with the '@State' property, allowing the app to observe changes made to the DataModel and update the app accordingly.
     @State var model:DataModel = DataModel()
     var body: some Scene {
         WindowGroup {
