5475633c65d196b776d6555d18db4ccf92baa6fd
Samuel Lee <samuellee249@gmail.com>
2023-04-19 13:56:47 +1000

    Added and finished documentation for the EditView

--- Checklish/EditView.swift
+++ Checklish/EditView.swift
@@ -7,9 +7,13 @@
 
 import SwiftUI
 
+/// The EditView struct is a view component that displays an editable text field alongside a cancel button. Furthermore, the EditView makes use of the editMode environment variable to determine whether edit mode is currently active. If edit mode is actuve, the view shows the text field along with the cancel button and edit mode icon. Ultimately, the EditView allows the user to edit an item.
 struct EditView: View {
+    /// A binding variable representing the text of the item being edited
     @Binding var item: String
+    /// A state variable representing the text to be displayed in the edit mode text field. Initially this string is empty.
     @State var displayItem: String = ""
+    /// An environment variable called editMode. This is provided by SwiftUI and is used to determing whether edit mode is active.
     @Environment(\.editMode) var editMode // Edit mode environment variable
 
     var body: some View {
